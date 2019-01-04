e22968dec1d060f69118870279fab41bf7fad7c2
Raul Renteria <raul.renteria@iest.edu.mx>
2019-01-03 21:52:53 -0600

    rutas hechas

--- iron/app-iron-sabado/routes/estadoresultados.js
+++ iron/app-iron-sabado/routes/estadoresultados.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const Activo = require("../models/Activo");
+const EstadoResultados = require("../models/EstadoResultados");
 const auth = require("../helpers/auth");
 
 //=========>     PONER MIDDLEWARE VERYFY.TOKEN "auth.verifyToken"
 router.get("/",  (req, res )=>{
-    Activo.find()
+  EstadoResultados.find()
     .populate('_negocio')
-    .then( activo => {
-        res.status(200).json({activo})
+    .then( estadoresultados => {
+        res.status(200).json({estadoresultados})
     })
     .catch(err =>{
         res.status(404).json({err, msg:"No hay sesión de usuario iniciada"})
@@ -16,82 +16,80 @@ router.get("/",  (req, res )=>{
 })
 
 router.post("/new", auth.verifyToken, (req, res) =>{
-        const newActivo = new Activo({
+        const newEstadoResultados = new EstadoResultados({
             _negocio: req.body._negocio,
-            //cuentas del activo
-          
+            //cuentas del estado de resultados
 /* 
 
-efectivo
-clientesdxcCP: {
-activoNoFinCorr: {
-inventariosCorrientes:{
-clientesdxcLP: {
-activoFinNoCorr: {
-inversiones: {
-propiedadesPlantasEquipo: {
-propiedadDeInversiones: {
-impuestosDiferidos: {
-sumaActivo
+ventas: req.body.ventas,
+  costoVentas: req.body.costoVentas,
+utilidadBruta: req.body.ventas - req.body.costoVentas,
+  gastosAdmn: req.body.gastosAdmn,
+  gastosOperativos: req.body.gastosOperativos,
+  gastosVentas: req.body.gastosVentas,
+utilidadOperativa: utilidadBruta - req.body.gastosAdmn - req.body.gastosOperativos - req.body.gastosVentas,
+  gastosFinancieros: req.body.gastosFinancieros,
+  ingresosFinancieros: req.body.ingresosFinancieros,
+utilidadAntesImpuestos: utilidadOperativa - req.body.gastosFinancieros + req.body.ingresosFinancieros,
+  impuestos: utilidadAntesImpuestos * .3,
+utilidadNeta:  utilidadAntesImpuestos - impuestos
 
 */
 
-            efectivo: req.body.efectivo,
-            clientesdxcCP: req.body.clientesdxcCP,
-            activoNoFinCorr: req.body.activoNoFinCorr,
-            inventariosCorrientes: req.body.inventariosCorrientes,
-            clientesdxcLP: req.body.clientesdxcLP,
-            activoFinNoCorr: req.body.activoFinNoCorr,
-            inversiones: req.body.inversiones,
-            propiedadesPlantasEquipo: req.body.propiedadesPlantasEquipo,
-
-
-
-
-
-            sumaActivo: req.body.efectivo + req.body.clientesdxcCP + req.body.activoNoFinCorr + req.body.inventariosCorrientes + req.body.clientesdxcLP + req.body.activoFinNoCorr + req.body.inversiones + req.body.propiedadesPlantasEquipo//SUMA FINAL, TODAS LAS CUENTAS          
-        });
-        newActivo.save()
-        .then(newActivo => {
-            res.status(200).json({newActivo})
-        })
-        .catch(err =>{
-            res.status(404).json({err, msg:"No hay sesión de usuario iniciada"})
-        })
+ventas: req.body.ventas,
+  costoVentas: req.body.costoVentas,
+utilidadBruta: req.body.ventas - req.body.costoVentas,
+  gastosAdmn: req.body.gastosAdmn,
+  gastosOperativos: req.body.gastosOperativos,
+  gastosVentas: req.body.gastosVentas,
+utilidadOperativa: utilidadBruta - req.body.gastosAdmn - req.body.gastosOperativos - req.body.gastosVentas,
+  gastosFinancieros: req.body.gastosFinancieros,
+  ingresosFinancieros: req.body.ingresosFinancieros,
+utilidadAntesImpuestos: utilidadOperativa - req.body.gastosFinancieros + req.body.ingresosFinancieros,
+  impuestos: utilidadAntesImpuestos * .3,
+utilidadNeta:  utilidadAntesImpuestos - impuestos
+});
+newEstadoResultados.save()
+.then(newEstadoResultados => {
+    res.status(200).json({newEstadoResultados})
+})
+.catch(err =>{
+    res.status(404).json({err, msg:"No hay sesión de usuario iniciada"})
+})
 })
 
 //READ
 router.get("/:id", (req, res)=>{
-    Activo.findById(req.params.id)
-    .populate('_negocio')
-    .then(activo =>{
-        res.status(201).json({activo})
-    })
-    .catch(err =>{
-        res.status(500).json({err, msg:"No se pudo leer"})
-    })
+EstadoResultados.findById(req.params.id)
+.populate('_negocio')
+.then(estadoresultados =>{
+res.status(201).json({estadoresultados})
+})
+.catch(err =>{
+res.status(500).json({err, msg:"No se pudo leer"})
+})
 })
 
 //UPDATE
 router.patch("/:id", (req, res) =>{
-    Activo.findByIdAndUpdate(req.params.id, req.body, {new: true})
-        .then(activo =>{
-            res.status(200).json({activo})
-        })
-        .catch(err =>{
-            res.status(500).json({err, msg:"No se pudo actualizar"})
-        })
+EstadoResultados.findByIdAndUpdate(req.params.id, req.body, {new: true})
+.then(estadoresultados =>{
+    res.status(200).json({estadoresultados})
+})
+.catch(err =>{
+    res.status(500).json({err, msg:"No se pudo actualizar"})
+})
 })
 //DELETE
 
 router.delete("/:id", auth.verifyToken, (req, res)=> {
-    Activo.findByIdAndDelete(req.params.id)
-    .then(activo=>{
-        res.status(200).json({activo})
-    })
-    .catch(err =>{
-        res.status(500).json({err, msg:"No se pudo eliminar"})
-    })
+EstadoResultados.findByIdAndDelete(req.params.id)
+.then(estadoresultados=>{
+res.status(200).json({estadoresultados})
+})
+.catch(err =>{
+res.status(500).json({err, msg:"No se pudo eliminar"})
+})
 })                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
 
 module.exports = router;
